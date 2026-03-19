24faef8764efc390ce0878af07a6dc9a8425cba5
Gnik Noiprocs <thanhnam.csc@gmail.com>
2026-03-19 23:30:47 +0700

    Added rat

---
+++ core/src/main/java/com/noiprocs/ui/libgdx/sprite/mob/RatSprite.java
@@ -0,0 +1,64 @@
+package com.noiprocs.ui.libgdx.sprite.mob;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.noiprocs.core.common.Vector3D;
+import com.noiprocs.core.model.Model;
+import com.noiprocs.core.model.mob.MobModel;
+import com.noiprocs.gameplay.model.action.ChargeRetreatAction;
+import com.noiprocs.gameplay.model.mob.RatModel;
+import com.noiprocs.ui.libgdx.sprite.LibgdxRenderContext;
+import com.noiprocs.ui.libgdx.sprite.LibgdxTexture;
+
+public class RatSprite extends MobSprite {
+  private static final String MODEL_CLASS = RatModel.class.getName();
+
+  private static final LibgdxTexture SW_TEXTURE = loadTexture(MODEL_CLASS, "sw");
+  private static final LibgdxTexture SE_TEXTURE = SW_TEXTURE.flipped();
+  private static final LibgdxTexture NW_TEXTURE = loadTexture(MODEL_CLASS, "nw");
+  private static final LibgdxTexture NE_TEXTURE = NW_TEXTURE.flipped();
+
+  public RatSprite() {
+    super(SW_TEXTURE);
+  }
+
+  @Override
+  public LibgdxTexture getTexture(Model model) {
+    MobModel mobModel = (MobModel) model;
+    Vector3D facingDirection = mobModel.getFacingDirection();
+
+    if (facingDirection.x + facingDirection.y < 0) {
+      if (facingDirection.y < 0) return NW_TEXTURE;
+      return NE_TEXTURE;
+    }
+    if (facingDirection.y > 0) return SE_TEXTURE;
+    return SW_TEXTURE;
+  }
+
+  @Override
+  public void render(
+      SpriteBatch batch,
+      Model model,
+      Model playerModel,
+      float offsetX,
+      float offsetY,
+      LibgdxRenderContext ctx) {
+    super.render(batch, model, playerModel, offsetX, offsetY, ctx);
+
+    MobModel mobModel = (MobModel) model;
+    if (!(mobModel.getAction() instanceof ChargeRetreatAction)) return;
+
+    ChargeRetreatAction chargeAction = (ChargeRetreatAction) mobModel.getAction();
+    LibgdxTexture tex = getTexture(model);
+    float[] b = screenBounds(tex, model, offsetX, offsetY, ctx);
+    float alpha =
+        ctx.alphaResolver.resolve(model, playerModel, b[0], b[0] + b[2], b[1], b[1] + b[3]);
+
+    if (chargeAction.isPreparing()) {
+      batch.setColor(1f, 0.85f, 0.1f, 0.45f * alpha);
+    } else {
+      batch.setColor(0.7f, 0.1f, 1f, 0.55f * alpha);
+    }
+    batch.draw(tex.textureRegion, b[0], b[1], b[2], b[3]);
+    batch.setColor(1f, 1f, 1f, 1f);
+  }
+}
